ae0d72d623d0ddd7044da7166461bc7aea03038b
Ethan Zhang <ezsland05@gmail.com>
2022-06-09 23:36:11 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# power-profiles-applet
+
+![](./screenshots/demo.png)
+
+power-profiles-applet is a simple gtk-based systray applet that lets
+you check and change your current power profile (using [power-profiles-daemon](https://gitlab.freedesktop.org/hadess/power-profiles-daemon))
+
+## Build:
+
+This program is written in [Rust](https://www.rust-lang.org/). The Rust compiler can be installed by following the
+instructions on the [official download page](https://www.rust-lang.org/tools/install).
+
+Install `gtk-rs` by following the instructions
+in [the book](https://gtk-rs.org/gtk4-rs/stable/latest/book/installation.html).
+
+```shell
+# Clone this repo
+$ git clone https://github.com/edzdez/power-profiles-applet
+
+# Build with cargo
+$ cargo build --release
+$ ./target/release/power-profiles-applet
+
+# Alternatively, build and run in one step
+$ cargo run --release
+```
---
+++ src/main.rs
@@ -0,0 +1,91 @@
+use std::process::Command;
+
+use gtk::prelude::*;
+use libappindicator::{AppIndicator, AppIndicatorStatus};
+
+fn get_current_power_profile() -> String {
+    let output = Command::new("powerprofilesctl")
+        .arg("get")
+        .output()
+        .expect("Failed to call powerprofilesctl")
+        .stdout;
+
+    String::from_utf8(output).unwrap().trim().to_string()
+}
+
+fn get_all_power_profiles() -> Vec<String> {
+    let output = Command::new("powerprofilesctl")
+        .arg("list")
+        .output()
+        .expect("Failed to call powerprofilesctl")
+        .stdout;
+
+    let output = String::from_utf8(output).unwrap();
+    output
+        .trim()
+        .split("\n\n")
+        .map(|output| {
+            let (a, _) = output.split_once('\n').unwrap();
+            let a = a.replace('*', "").replace(' ', "");
+            a[..a.len() - 1].trim().to_string()
+        })
+        .collect()
+}
+
+fn set_power_profile(label: &str) {
+    Command::new("powerprofilesctl")
+        .args(["set", label])
+        .spawn()
+        .expect("Failed to call powerprofilesctl");
+}
+
+fn create_menu() -> gtk::Menu {
+    let menu = gtk::Menu::builder().halign(gtk::Align::Start).build();
+
+    let current_profile_label = gtk::MenuItem::builder()
+        .label("Power Profiles: ")
+        .halign(gtk::Align::Start)
+        .sensitive(false)
+        .build();
+
+    let divider = gtk::SeparatorMenuItem::new();
+
+    menu.append(&current_profile_label);
+    menu.append(&divider);
+
+    let current_profile = get_current_power_profile();
+    let profiles = get_all_power_profiles();
+
+    let mut group = None;
+    for profile in profiles {
+        let profile_button = gtk::RadioMenuItem::builder().label(&profile).build();
+        profile_button.connect_activate(|b| {
+            dbg!("setting power profile to {}", b.label().unwrap());
+            set_power_profile(&b.label().unwrap());
+        });
+
+        match group {
+            Some(ref group) => profile_button.join_group(Some(group)),
+            None => group = Some(profile_button.clone()),
+        }
+        if profile == current_profile {
+            profile_button.activate();
+        }
+        menu.append(&profile_button);
+    }
+
+    menu
+}
+
+fn main() {
+    gtk::init().expect("Failed to init gtk");
+
+    let mut indicator = AppIndicator::new("Power Profiles Indicator", "battery-good");
+    indicator.set_status(AppIndicatorStatus::Active);
+
+    let mut menu = create_menu();
+
+    indicator.set_menu(&mut menu);
+    menu.show_all();
+    gtk::main();
+}
